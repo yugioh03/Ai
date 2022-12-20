8cd8dc0f0c7658e92f874f50712604bead6b36c1
R.V <raimunds.vanags@inbox.lv>
2022-12-20 13:35:56 +0200

    l

--- index.js
+++ index.js
@@ -2,17 +2,9 @@ const express = require ('express');
 const dotnenv = require ('dotenv').config();
 const port = process.env.PORT || 5000;
 const path = require('path')
-
-
 const app = express ();
-
-
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.use('/openai', require('./routes/openaiRoutes'));
-
-
 app.listen(port, () =>console.log(`Server started on port ${port}`));
